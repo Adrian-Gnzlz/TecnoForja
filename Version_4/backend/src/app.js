8f82de35f80c0adbf997f9be382a3729f9ea615e
Adrian Gonzalez <fentuare@gmail.com>
2025-11-30 19:29:34 -0600

    cambios 2

--- Version_4/backend/src/app.js
+++ Version_4/backend/src/app.js
@@ -25,5 +25,5 @@ app.use("/api/pagos", paymentsRoutes);
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor TecnoForja API escuchando en http://localhost:${PORT}`);
+    console.log(`Servidor TecnoForja API escuchando en el puerto ${PORT}`);
 });

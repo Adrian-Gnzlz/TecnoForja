fa9397b21bae0c5c4151f7fb4b224dbd6fc498c9
Adrian Gonzalez <fentuare@gmail.com>
2025-11-30 21:34:02 -0600

    cambios 4

--- Version_4/frontend/script.js
+++ Version_4/frontend/script.js
@@ -1,6 +1,6 @@
 // frontend/script.js
 
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = "https://tecnoforja-production.up.railway.app/api";
 
 const cart = [];
 const productsLocal = [
